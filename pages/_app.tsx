c57db52146c77a5b86258a4b809ac965c82dca9e
Robert Guss <robertguss@pm.me>
2021-12-01 16:28:19 -0500

    cleaned up app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,8 @@
 import "tailwindcss/tailwind.css"
 import "../styles/global.css"
 import Head from "next/head"
-import { useEffect } from "react"
-import * as FullStory from "@fullstory/browser"
 
 function MyApp({ Component, pageProps }) {
-  // @ts-ignore
-  if (typeof window !== "undefined" && !window.Cypress) {
-    // eslint-disable-next-line
-    useEffect(() => {
-      FullStory.init({ orgId: process.env.NEXT_PUBLIC_FULL_STORY_ORG_ID })
-    }, [])
-  }
-
   return (
     <>
       <Head>
@@ -20,11 +10,6 @@ function MyApp({ Component, pageProps }) {
         <link
           href="https://cdnjs.cloudflare.com/ajax/libs/prism/1.23.0/themes/prism-tomorrow.min.css"
           rel="stylesheet"
-        />
-        <link
-          rel="preconnect"
-          href="https://8V1MWQUQBN-dsn.algolia.net"
-          crossOrigin=""
         />
       </Head>
       <Component {...pageProps} />
