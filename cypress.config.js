eb046deffe4f7eb080a257fa8f7d95528cd44785
Robert Guss <robertguss@pm.me>
2022-05-13 10:30:40 -0400

    added cypress v10 and supporting files. E2E testing is working

---
+++ cypress.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from "cypress"
+
+export default defineConfig({
+  component: {
+    devServer: {
+      framework: "next",
+      bundler: "webpack",
+    },
+  },
+
+  e2e: {
+    baseUrl: "http://localhost:3000",
+    specPattern: "cypress/integration/**/*.spec.{js,jsx,ts,tsx}",
+    viewportHeight: 1000,
+    viewportWidth: 1280,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+})
