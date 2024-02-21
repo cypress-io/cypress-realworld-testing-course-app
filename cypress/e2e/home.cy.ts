c27e8410a75d116b29db76b2a84f1590ae3be279
GUSTAVO FERRAZ SANTOS <110568302+gustavoferrazdesousa@users.noreply.github.com>
2024-02-20 21:28:17 -0300

    teste de inscrição em lista de email

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -4,10 +4,11 @@ describe("home page", () => {
   })
 
   it("the h1 contains the correct text", () => {
-    cy.get("[data-test='hero-heading']").contains(
+    cy.getByData("hero-heading").contains(
       "Testing Next.js Applications with Cypress"
     )
   })
+
   it("the features on the homepage are correct", () => {
     cy.get("dt").eq(0).contains("4 Courses")
     cy.get("dt").eq(1).contains("25+ Lessons")
