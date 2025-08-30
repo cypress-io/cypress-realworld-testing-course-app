f1b2dbe37dcb8b4c4a0747736496f3c8ecddba8d
bogs <bogs@keba.com>
2025-08-30 15:01:43 +0200

    Update general-tests.cy.ts file

--- cypress/e2e/general-tests.cy.ts
+++ cypress/e2e/general-tests.cy.ts
@@ -1,6 +1,6 @@
 describe("template spec", () => {
   it("TC1: Load via custom command", () => {
-    cy.loadApp(1000) // Custom command to load the app with a timeout
+    cy.loadApp(5000) // Custom command to load the app with a timeout
     cy.contains("Testing Next.js Applications with Cypress")
   })
 
